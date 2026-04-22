38bfad0919a62018a2038b264f6c0301e2cb2d92
manacoffee <brisbane@manacoffee.net>
2026-04-22 11:08:32 +1000

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const path = require('path');
 const { randomUUID } = require('crypto');
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
